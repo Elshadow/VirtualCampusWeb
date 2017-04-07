dcbf70b54e809c48a5842bfa3f23b06ba2d021a6
guohzou <guoz@idreamtech.com.cn>
2017-04-07 17:45:32 +0800

    修改bug

--- www/wiki/js/app.js
+++ www/wiki/js/app.js
@@ -21,3 +21,16 @@ var config = {
     modulePageUrlPrefix:'/wiki/module',
     moduleApiUrlPrefix:'http://localhost:8099/api/module/',  // + moduleName + "/models/" + modelName + '[apiName]'
 };
+//监控页面是否渲染完毕
+app.directive('onFinishRenderFilters', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function() {
+                    scope.$emit('ngRepeatFinished');
+                });
+            }
+        }
+    };
+});
