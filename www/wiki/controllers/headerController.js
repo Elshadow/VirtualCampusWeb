e040fded1f13bbf7ffa3aaa8f4163c2ea50246b7
houc <houc@idreamtech.com.cn>
2017-05-11 13:40:31 +0800

    转换操作添加确认提示，并刷新页面的操作

--- www/wiki/controllers/headerController.js
+++ www/wiki/controllers/headerController.js
@@ -44,40 +44,42 @@ app.controller('headerCtrl', function ($scope, $state, $auth, Account, Message) 
     }
 
     $scope.coordinateTrans = function () {
-        // 提供转换WGS84经纬度坐标为百度经纬度坐标功能实现
-        util.http("get", config.apiUrlPrefix + 'school', {}, function (data) {
-            $scope.schools = data;
-            // $scope.$apply();
-            $scope.schools.map(function(item,index){
-                if (item.norhtWestLng != null && typeof(item.norhtWestLng) != "undefined" && item.schoolName != null) {
-                    var northWest = $scope.coordinateTransform(item.norhtWestLng, item.northWestLat);
-                    var northEast = $scope.coordinateTransform(item.northEastLng, item.northEastLat);
-                    var southEast = $scope.coordinateTransform(item.southEastLng, item.southEastLat);
-                    var southWest = $scope.coordinateTransform(item.southWestLng, item.southWestLat);
-                    var params = {
-                        schoolName:item.schoolName,
-                        _id:item._id,
-                        northWestLat:northWest[1],
-                        norhtWestLng:northWest[0],
-                        northEastLat:northEast[1],
-                        northEastLng:northEast[0],
-                        southWestLat:southWest[1],
-                        southWestLng:southWest[0],
-                        southEastLat:southEast[1],
-                        southEastLng:southEast[0],
-                    }
-                    util.http("put", config.apiUrlPrefix + 'school', params, function (data) {
-                        // 更新学校区域范围经纬度为百度经纬度
+        if (confirm("确定执行此操作,转换OSM经纬度坐标为百度经纬度坐标吗(该操作仅限执行一次,请谨慎操作,如造成数据错乱后果自负)?")) {
+            // 提供转换WGS84经纬度坐标为百度经纬度坐标功能实现
+            util.http("get", config.apiUrlPrefix + 'school', {}, function (data) {
+                $scope.schools = data;
+                // $scope.$apply();
+                $scope.schools.map(function(item,index){
+                    if (item.norhtWestLng != null && typeof(item.norhtWestLng) != "undefined" && item.schoolName != null) {
+                        var northWest = $scope.coordinateTransform(item.norhtWestLng, item.northWestLat);
+                        var northEast = $scope.coordinateTransform(item.northEastLng, item.northEastLat);
+                        var southEast = $scope.coordinateTransform(item.southEastLng, item.southEastLat);
+                        var southWest = $scope.coordinateTransform(item.southWestLng, item.southWestLat);
+                        var params = {
+                            schoolName:item.schoolName,
+                            _id:item._id,
+                            northWestLat:northWest[1],
+                            norhtWestLng:northWest[0],
+                            northEastLat:northEast[1],
+                            northEastLng:northEast[0],
+                            southWestLat:southWest[1],
+                            southWestLng:southWest[0],
+                            southEastLat:southEast[1],
+                            southEastLng:southEast[0],
+                        }
+                        util.http("put", config.apiUrlPrefix + 'school', params, function (data) {
+                            // 更新学校区域范围经纬度为百度经纬度
 
-                    }, function (error) {
-                        $scope.errMsg = error.message;
-                    });
-                }
-            })
-            // console.log($scope.searchName);
-        }, function (error) {
-            $scope.errMsg = error.message;
-        });
+                        }, function (error) {
+                            $scope.errMsg = error.message;
+                        });
+                    }
+                })
+                // console.log($scope.searchName);
+            }, function (error) {
+                $scope.errMsg = error.message;
+            });
+        }
     }
 
     $scope.$on("onUserProfile", function (event, user) {
